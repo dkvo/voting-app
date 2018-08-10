418f52c836d9871424987c5b6187122fa78a844a
dkvo <dangkhoanvo@gmail.com>
2018-08-10 01:54:36 -0700

    allow cross origin resource sharing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const https = require('https');
+var cors = require('cors')
 const bodyParser = require('body-parser');
 
 const db = require('./db/db');
@@ -12,6 +13,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/polls', pollRouter);
 app.use('/users', userRouter);
 
